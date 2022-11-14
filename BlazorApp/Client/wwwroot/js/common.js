3b4d49c4474ebc7bfd7de23bc73e8fd24d19abbe
lay_kim <45022305+Kimyoungin1@users.noreply.github.com>
2022-11-14 13:04:41 +0900

    [Temp] Portfolio Design 1 - Lay

--- BlazorApp/Client/wwwroot/js/common.js
+++ BlazorApp/Client/wwwroot/js/common.js
@@ -1,3 +1,35 @@
-﻿function Message(msg) {
-    return `javascript message, ${mesage}`;
+﻿
+
+function Collapse() {
+    $('.sidebar').css('display', 'none');
+    $('#top').css('display', 'none');
 }
+
+function ScrollIntoView(target) {
+    const element = document.getElementById(target);
+
+    element.scrollIntoView();
+}
+
+const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+}
+
+(function () {
+    let navbarlinks = select('#navbar .scrollto', true)
+    const navbarlinksActive = () => {
+        let position = window.scrollY + 200
+        navbarlinks.forEach(navbarlink => {
+            if (!navbarlink.hash) return
+            let section = select(navbarlink.hash)
+            if (!section) return
+            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+                navbarlink.classList.add('active')
+            } else {
+                navbarlink.classList.remove('active')
+            }
+        })
+    }
+    window.addEventListener('load', navbarlinksActive)
+    onscroll(document, navbarlinksActive)
+});
