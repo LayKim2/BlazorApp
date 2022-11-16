a87eb25a27c13daafbdaf649f07d6d33cec1494b
lay_kim <45022305+Kimyoungin1@users.noreply.github.com>
2022-11-16 20:54:22 +0900

    Upload logic for sidebar data - Lay

---
+++ BlazorApp/Server/Migrations/20221116093019_UpdatePortfolio5.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorApp.Server.Migrations
+{
+    public partial class UpdatePortfolio5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CamereTestUrl",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SideImageData",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SideImageUrl",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CamereTestUrl",
+                table: "Portfolios");
+
+            migrationBuilder.DropColumn(
+                name: "SideImageData",
+                table: "Portfolios");
+
+            migrationBuilder.DropColumn(
+                name: "SideImageUrl",
+                table: "Portfolios");
+        }
+    }
+}
--- BlazorApp/Server/Services/PortfolioService/PortfolioService.cs
+++ BlazorApp/Server/Services/PortfolioService/PortfolioService.cs
@@ -42,6 +42,23 @@ public class PortfolioService : IPortfolioService
             dbPortfolio.SideName = portfolio.SideName;
             dbPortfolio.Concept1 = portfolio.Concept1;
             dbPortfolio.Concept2 = portfolio.Concept2;
+            dbPortfolio.DateUpdated = DateTime.Now;
+            dbPortfolio.AboutBirthday = portfolio.AboutBirthday;
+            dbPortfolio.AboutIntroduce = portfolio.AboutIntroduce;
+            dbPortfolio.AboutSize = portfolio.AboutSize;
+            dbPortfolio.AboutAge = portfolio.AboutAge;
+            dbPortfolio.CameraTestDateUpdated = portfolio.CameraTestDateUpdated;
+            dbPortfolio.HighlightDateUpdated = portfolio.HighlightDateUpdated;
+            dbPortfolio.RightEnglishName = portfolio.RightEnglishName;
+            dbPortfolio.RightName = portfolio.RightName;
+            dbPortfolio.AboutEmail = portfolio.AboutEmail;
+            dbPortfolio.AboutMainFilmograpy1 =  portfolio.AboutMainFilmograpy1;
+            dbPortfolio.AboutMainFilmograpy2 = portfolio.AboutMainFilmograpy2;
+            dbPortfolio.AboutMainFilmograpy3 =  portfolio.AboutMainFilmograpy3;
+            dbPortfolio.AboutMainFilmograpy4 =  portfolio.AboutMainFilmograpy4;
+            dbPortfolio.AboutPhone = portfolio.AboutPhone;
+            dbPortfolio.CamereTestUrl  =  portfolio.CamereTestUrl;
+            dbPortfolio.SideImageData =  portfolio.SideImageData;
 
             response.Data = portfolio;
         }
