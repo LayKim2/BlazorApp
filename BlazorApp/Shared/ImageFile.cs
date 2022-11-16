7a9a542c66da99b238079625d0b51b9729e3d754
LayKim2 <102665441+LayKim2@users.noreply.github.com>
2022-11-16 16:23:01 +0900

    [Test] Upload Video logic - Lay

--- BlazorApp/Shared/ImageFile.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlazorApp.Shared;
-
-public class ImageFile
-{
-    public string base64data { get; set; }
-    public string contentType { get; set; }
-    public string fileName { get; set; }
-}
--- BlazorApp/Shared/Portfolio.cs
+++ BlazorApp/Shared/Portfolio.cs
@@ -8,11 +8,24 @@ public class Portfolio
     public User? User { get; set; }
     public int UserId { get; set; }
     public string Email { get; set; } = string.Empty;
+
+    // sidebar section
     public string SideName { get; set; } = string.Empty;
+    public string SideImageUrl { get; set; } = string.Empty;
+    public string SideImageData { get; set; } = string.Empty;
+    // sidebar section
+
+
+    // home section
     public string RightName { get; set; } = string.Empty;
     public string RightEnglishName { get; set; } = string.Empty;
+    public string CamereTestUrl { get; set; } = string.Empty;
     public DateTime CameraTestDateUpdated { get; set; } = DateTime.Now;
     public DateTime HighlightDateUpdated { get; set; } = DateTime.Now;
+    // home section
+
+
+    //about section
     public string AboutIntroduce { get; set; } = string.Empty;
     public string AboutBirthday{ get; set; } = string.Empty;
     public string AboutPhone { get; set; } = string.Empty;
@@ -23,10 +36,15 @@ public class Portfolio
     public string AboutMainFilmograpy2 { get; set; } = string.Empty;
     public string AboutMainFilmograpy3 { get; set; } = string.Empty;
     public string AboutMainFilmograpy4 { get; set; } = string.Empty;
+    //about section
+
+    //concept 1 section
     public string Concept1 { get; set; } = string.Empty;
+    //concept 1 section
+
+    //concept 2 section
     public string Concept2 { get; set; } = string.Empty;
-    public string SideImageUrl { get; set; } = string.Empty;
-    public string SideImageData { get; set; } = string.Empty;
+    //concept 2 section
 
     public DateTime DateUpdated { get; set; } = DateTime.Now;
 }
