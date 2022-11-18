d962a5643fde9d4c3d0c4f46c16888f50ba8f7ab
lay_kim <45022305+Kimyoungin1@users.noreply.github.com>
2022-11-18 15:43:43 +0900

    Added ApplyTo Page - Lay

---
+++ BlazorApp/Shared/Apply.cs
@@ -0,0 +1,11 @@
+﻿
+namespace BlazorApp.Shared;
+
+public class Apply
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    public string Result { get; set; } = string.Empty;
+    public string Note { get; set; } = string.Empty;
+}
