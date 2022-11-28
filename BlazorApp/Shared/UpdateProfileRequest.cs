1ab18da498da16af8feaf9b5c875580a4541e882
LayKim2 <102665441+LayKim2@users.noreply.github.com>
2022-11-28 16:24:17 +0900

    Added account - profile UI - Lay

---
+++ BlazorApp/Shared/UpdateProfileRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Shared;
+
+public class UpdateProfileRequest
+{
+    [Required]
+    public string UserName { get; set; }
+    public string UserEnglishName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+}
